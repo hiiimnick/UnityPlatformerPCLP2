0d45522ff8696b7f84ebc44f1d04868527016eec
hiiimnick <95238421+hiiimnick@users.noreply.github.com>
2024-05-20 22:28:41 +0300

    adaugat detectie pereti

--- Assets/Scripts/miscareJucator.cs
+++ Assets/Scripts/miscareJucator.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class miscareJucator : MonoBehaviour
 {
     [SerializeField] private float viteza;
+    [SerializeField] private LayerMask layerPamant; //adaugat layer pentru a verifica daca jucatorul atinge pamantul
+    [SerializeField] private LayerMask layerPerete; //adaugat layer pentru a verifica daca jucatorul atinge un perete
+
+    private BoxCollider2D boxCollider;
     private Rigidbody2D corp;
     private Animator animatie;
-    private bool peTeren;
 
     private void Awake() //de fiecare data cand pornesc jocul, se executa scriptul
     {
         corp = GetComponent<Rigidbody2D>();
         animatie = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update() //se executa la fiecare frame
@@ -24,23 +28,27 @@ public class miscareJucator : MonoBehaviour
         else if(miscare < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space) && peTeren)
+        if (Input.GetKey(KeyCode.Space) && pePamant())
             Saritura();
 
         animatie.SetBool("Alergare", miscare != 0); //verifica daca jucatorul se misca si schimba animatia corespunzator
-        animatie.SetBool("peTeren", peTeren);
+        animatie.SetBool("peTeren", pePamant());
     }
+
+    private bool pePamant()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, layerPamant);
+        return hit.collider != null; // daca jucatorul atinge pamantul, returneaza true altfel false
+    }
+
     private void Saritura()
     {
         corp.velocity = new Vector2(corp.velocity.x, viteza);
-        peTeren = false;
+    }
+    private bool perete()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, new Vector2(transform.localScale.x, 0), 0.1f, layerPerete);
+        return hit.collider != null; // daca jucatorul atinge un perete, returneaza true altfel false
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Teren")
-        {
-            peTeren = true;
-        }
-    }
 }
